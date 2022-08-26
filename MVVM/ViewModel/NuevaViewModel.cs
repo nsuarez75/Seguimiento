a090ec2ab81aefa03bddb8967ffb7d624b3f8af7
diggythecandygun <suarez7592@hotmail.com>
2022-08-26 20:37:51 +0200

    upd

--- MVVM/ViewModel/NuevaViewModel.cs
+++ MVVM/ViewModel/NuevaViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Seguimiento.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace Seguimiento.MVVM.ViewModel
 {
-    class NuevaViewModel
+    class NuevaViewModel : ObservableObject
     {
+
+        private string _texto;
+
+        public string Texto
+        {
+            get { return _texto; }
+            set 
+            { 
+                _texto = value;
+                OnPropertyChanged();         
+            }
+        }
+
+
+
+
+        public NuevaViewModel()
+        {
+            Texto = "probabando";
+        }
+
+
     }
 }
