237007547a6ced6d2ac6cde741a1b9e339fcf2ac
diggythecandygun <suarez7592@hotmail.com>
2022-09-03 15:22:31 +0200

    upd

--- MVVM/View/AjustesView.xaml.cs
+++ MVVM/View/AjustesView.xaml.cs
@@ -24,5 +24,6 @@ namespace Seguimiento.MVVM.View
         {
             InitializeComponent();
         }
+
     }
 }
--- MVVM/ViewModel/AjustesViewModel.cs
+++ MVVM/ViewModel/AjustesViewModel.cs
@@ -4,22 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Command = MvvmHelpers.Commands.Command;
 
 namespace Seguimiento.MVVM.ViewModel
 {
     class AjustesViewModel : MvvmHelpers.ObservableObject
     {
 
+        #region Propiedades
+
         public ObservableRangeCollection<String> ContenidoLista { get;}
+
+        public Command agregarPlantilla { get; }
+
+        #endregion
+
+
+        #region Metodos
+
+        void AgregarPlantilla()
+        {
+            //Todo: Añadir plantilla a la BBDD y actualizar la lista de plantillas
+
+            ActualizarPlantillas();
+        }
+
+        void ActualizarPlantillas()
+        {
+            //Todo: Actualizar la lista de plantillas con la BBDD
+        }
+
+        #endregion
 
         public AjustesViewModel()
         {
             ContenidoLista = new ObservableRangeCollection<string>();
-            ContenidoLista.Add("prueba1");
-            ContenidoLista.Add("prueba12");
 
+            agregarPlantilla = new Command(AgregarPlantilla);
 
         }
+
+
+
+
+
+
+
 
     }
 }
